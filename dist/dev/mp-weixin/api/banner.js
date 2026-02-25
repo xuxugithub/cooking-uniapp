189467936b9c3d657bbc4cfbea242725e80796b5
xuxugithub <56546995+xuxugithub@users.noreply.github.com>
2026-02-25 22:29:07 +0800

    Delete dist/dev/mp-weixin directory

--- dist/dev/mp-weixin/api/banner.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const utils_request = require("../utils/request.js");
-const getBannerList = () => {
-  return utils_request.get("/api/app/banner/list");
-};
-exports.getBannerList = getBannerList;
--- dist/dev/mp-weixin/api/category.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const utils_request = require("../utils/request.js");
-const getCategoryList = () => {
-  return utils_request.get("/api/app/category/list");
-};
-exports.getCategoryList = getCategoryList;
