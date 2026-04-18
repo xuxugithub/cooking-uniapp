299349488167381448d3ca7336afd538b1fe9f79
xuxugithub <56546995+xuxugithub@users.noreply.github.com>
2026-04-18 14:55:13 +0800

    Delete dist directory

--- dist/dev/mp-weixin/api/banner.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const utils_request = require("../utils/request.js");
-const getBannerList = (options = {}) => {
-  return utils_request.get("/api/app/banner/list", {}, options);
-};
-exports.getBannerList = getBannerList;
